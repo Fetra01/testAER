59f2f46e1fc8a00bbb48f9942be98d68e79739cf
Fetra01 <fetra.andria@outlook.fr>
2019-10-17 16:19:34 +0200

    créations Titre, question et proposition

--- test1/src/AppBundle/Controller/CreateController.php
+++ test1/src/AppBundle/Controller/CreateController.php
@@ -23,12 +23,15 @@ class CreateController extends Controller{
      * @param Request $request
      * @return Response
      */
+
+    // Creation du Titre du Questionnaire , question et proposition
     public function indexAction(Request $request){
         if($request->isMethod('GET')){
             return $this->render('Create/index.html.twig');
         }
         elseif ($request->isMethod('POST')){
-            $title = $request->request->get('title');
+
+            $title = $request->request->get( 'title');
             $quizz = array();
             $nbQuestion= $request->request->get('nbQuestion');
 
@@ -38,10 +41,9 @@ class CreateController extends Controller{
                 $prop2 = $request->request->get('prop2_'.$i);
                 $prop3 = $request->request->get('prop3_'.$i);
                 $prop4 = $request->request->get('prop4_'.$i);
-
                 $quizz[] = array(
                     'question' => $question,
-                    'propositions' => array($prop1,$prop2,$prop3,$prop4)
+                    'proposition' => array($prop1,$prop2,$prop3,$prop4)
                 );
             }
 
@@ -49,14 +51,15 @@ class CreateController extends Controller{
 
             $survey = new Survey();
             $survey->setTitle($title);
-
-            for($i=1; $i<=$nbQuestion; $i++){
-                $quizz = new Quizz();
-                $quizz->setQuestion($quizz[$i-1]['question']);
+            //Envoie du question et des propositions à la bdd
+            for($i=1; $i<=$nbQuestion; $i++) {
+                $quizz1 = new Quizz();
+                var_dump($quizz);
+                $quizz1->setQuestion($quizz[$i - 1]['question']);
                 //$quizz->setReponse($quizz[$i-1]['reponse']);
-                $quizz->setProposition($quizz[$i-1]['proposition']);
-                $quizz->setSurvey($survey);
-                $survey->getQuizz()->add($quizz);
+                $quizz1->setProposition($quizz[$i - 1]['proposition']);
+                $quizz1->setSurvey($survey);
+                $survey->getQuizz()->add($quizz1);
 
             }
 
@@ -69,7 +72,7 @@ class CreateController extends Controller{
                 $success = false;
                 $message = $e->getMessage();
             }
-
+            //Message d'erreur ou de succès
             if($success){
                 return $this->render('Create/creatResults.html.twig', array(
                     'success' => true
